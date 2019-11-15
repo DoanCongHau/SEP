8ccc61982e8deff5bb8790fcae2f817cd728110f
DoanCongHau <56835408+DoanCongHau@users.noreply.github.com>
2019-11-15 10:48:53 +0700

    Add files via upload

---
+++ SumFromAToB.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Công Hậu
+ */
+import java.util.Scanner;
+
+public class SumFromAToB {
+
+    public static void main(String[] args) {
+        int a, b, sum1 = 0, sum2 = 0;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("input a: ");
+        a = sc.nextInt();
+        System.out.println("input b: ");
+        b = sc.nextInt();
+
+        if (a < b) {
+            for (int i = a; i <= b; i++) {
+                sum1 += i;
+            }
+            System.out.println(sum1);
+        }
+        if (a > b) {
+            for (int i = b; i <= a; i++) {
+                sum2 += i;
+            }
+            System.out.println(sum2);
+        }
+        if (a == b) {
+            System.out.println(a);
+        }
+
+    }
+}
